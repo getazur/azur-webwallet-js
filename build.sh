427d9f91fd0b02e3ce0ca52becf46abb28ccfb67
getazur <45802475+getazur@users.noreply.github.com>
2019-01-19 01:34:42 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+rm /etc/nginx/sites-available/default
+mv default /etc/nginx/sites-available/
+nginx restart
 npm install
 node ./node_modules/typescript/bin/tsc --project tsconfig.prod.json
 node build.js
