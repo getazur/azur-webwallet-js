ee29f12c7c984e7535d59489887709f1bf4463f8
getazur <45802475+getazur@users.noreply.github.com>
2019-01-19 01:14:33 +0100

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -1,12 +1,1 @@
-wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get update -y
-sudo apt-get install npm redis-server apt-transport-https dotnet-sdk-2.1 -y
-wget https://getazur.org/linux-cli.tar.gz
-wget https://getazur.org/blockchain.tar.gz
-tar -xvf linux-cli.tar.gz
-tar -xvf blockchain.tar.gz
-rm linux-cli.tar.gz blockchain.tar.gz
-mv linux-cli/azurd /usr/bin
-mv root/blockchain/.Azur ~/
-rm -r linux-cli root
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb && dpkg -i packages-microsoft-prod.deb && apt-get update -y && apt-get install npm redis-server apt-transport-https dotnet-sdk-2.1 -y
