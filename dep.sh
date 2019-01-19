f47e7b696b72150ed35c770d4025577adff5eaf7
getazur <45802475+getazur@users.noreply.github.com>
2019-01-19 01:18:17 +0100

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -1,2 +1,2 @@
-wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb && dpkg -i packages-microsoft-prod.deb
-apt-get update -y && apt-get install npm redis-server apt-transport-https dotnet-sdk-2.1 -y
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb && dpkg -i packages-microsoft-prod.deb
+add-apt-repository universe && apt-get update -y && apt-get install apt-transport-https && apt-get install npm redis-server dotnet-sdk-2.1 -y
